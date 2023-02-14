238cd792c8c05238a72cdd379b39f0c5cd58b768
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-02-14 07:59:22 -0500

    how to join with route resource

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route as FacadesRoute;
 use Illuminate\Support\ServiceProvider;
+use Iluminate\Support\Facades\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        FacadesRoute::resourceVerbs([
+            'create' => 'crear',
+            'edit' => 'editar',
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\CursoController;
 |
 */
 
-// Route::get('/', HomeController::class);
+Route::get('/', HomeController::class);
 
 // Route::controller(CursoController::class)->group(function () {
 //     Route::get('cursos', 'index');
@@ -26,22 +26,24 @@ use App\Http\Controllers\CursoController;
 //     Route::get('cursos/{curso}', 'show');
 // });
 
-Route::get('/', HomeController::class);
+// Route::get('/', HomeController::class);
 
-Route::get('cursos', [CursoController::class, 'index'])->name('cursos.index');
+// Route::get('cursos', [CursoController::class, 'index'])->name('cursos.index');
 
-Route::get('cursos/create', [CursoController::class, 'create'])->name('cursos.create');
+// Route::get('cursos/create', [CursoController::class, 'create'])->name('cursos.create');
 
-Route::post('cursos', [CursoController::class, 'store'])->name('cursos.store');
+// Route::post('cursos', [CursoController::class, 'store'])->name('cursos.store');
 
-Route::get('cursos/{curso}', [CursoController::class, 'show'])->name('cursos.show');
+// Route::get('cursos/{curso}', [CursoController::class, 'show'])->name('cursos.show');
 
-Route::get('cursos/{curso}/edit', [CursoController::class, 'edit'])->name('cursos.edit');
+// Route::get('cursos/{curso}/edit', [CursoController::class, 'edit'])->name('cursos.edit');
 
-Route::put('cursos/{curso}', [CursoController::class, 'update'])->name('cursos.update');
+// Route::put('cursos/{curso}', [CursoController::class, 'update'])->name('cursos.update');
 
-Route::delete('cursos/{curso}', [CursoController::class, 'destroy'])->name('cursos.destroy');
+// Route::delete('cursos/{curso}', [CursoController::class, 'destroy'])->name('cursos.destroy');
 
+// Route::resource('asignaturas', CursoController::class)->parameters(['asignaturas' => 'curso'])->names('cursos');
+Route::resource('cursos', CursoController::class);
 
 // Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria= null) {
 
