31979e6f1f892affa6e0882005757da275919bb2
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-02-15 09:06:08 -0500

    working with email and platform fake mailtrap

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CursoController;
 
+use App\Mail\ContactanosMailable;
+use Illuminate\Support\Facades\Mail;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -46,6 +49,14 @@ Route::get('/', HomeController::class)->name('home');
 Route::resource('cursos', CursoController::class);
 
 Route::view('nosotros', 'nosotros')->name('nosotros');
+
+Route::get('contactanos', function () {
+    $correo = new ContactanosMailable;
+
+    Mail::to('rtcardenas26@gmail.com')->send($correo);
+
+    return "Mensaje enviado";
+});
 
 // Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria= null) {
 
